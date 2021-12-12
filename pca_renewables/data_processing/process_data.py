50608098a7bab8346340468efe9bd33e8a136086
jbarberia <jbarberia@frba.utn.edu.ar>
2021-12-12 08:21:30 -0300

    Proccess SMN data

--- pca_renewables/data_processing/process_data.py
+++ pca_renewables/data_processing/process_data.py
@@ -1,19 +1,27 @@
-import pandas as pd
 import os
-from os import listdir
 from datetime import datetime
+from functools import reduce
+from os import listdir
+
+import pandas as pd
+from pandas.core.algorithms import unique
+
 
 def read_files(cammesa_folder: str, smn_folder: str) -> pd.DataFrame:
     df_cammesa = parse_cammesa(cammesa_folder)
-    pass
-    # df_smn = parse_smn(smn_folder)
+    df_smn = parse_smn(smn_folder)
+    df = pd.merge(df_cammesa, df_smn, on="id", how="inner")
+    return df
+
 
 def is_file_in_path(folder:str):
     return lambda file: os.path.isfile(path(folder, file))
 
+
 def path(folder:str, file:str) -> str:
     path = "{}/{}".format(folder, file)
     return path
+
 
 def parse_cammesa(folder: str) -> pd.DataFrame:
     # Read all the file data
@@ -26,9 +34,12 @@ def parse_cammesa(folder: str) -> pd.DataFrame:
     # Concat the data
     df = pd.concat(data, ignore_index=True)
     # Sort by date
-    df.Momento = pd.to_datetime(df.Momento)
+    df["Momento"] = pd.to_datetime(df["Momento"])
     df.sort_values("Momento")
+    df["id"] = (df["Momento"].apply(lambda x: x.strftime("%d-%m-%y %H:%M")))
+    df = df.drop("Momento", axis=1)
     return df
+
 
 def parse_smn(folder: str) -> pd.DataFrame:
     # Read all the file data
@@ -46,7 +57,23 @@ def parse_smn(folder: str) -> pd.DataFrame:
     to_str = lambda x: str(int(x))
     df["Momento"] = df["HORA"].apply(to_str) + "-" + df["FECHA"].apply(to_str)
     df["Momento"] = df["Momento"].apply(lambda x: datetime.strptime(x, "%H-%d%m%Y"))
+    df["id"] = (df["Momento"].apply(lambda x: x.strftime("%d-%m-%y %H:%M")))
     # drop unuseful columns
-    df.drop(["FECHA", "HORA"], axis=1)
-
-    return df
+    df = df.drop(["FECHA", "HORA", "Momento"], axis=1)
+    # Transpose data, make columns per station
+    stations = unique(df["NOMBRE"])
+    dfs_for_stations = []
+    for station in stations:
+        # Sub-df per station
+        station_df = df[df["NOMBRE"] == station]
+        station_df = station_df.drop("NOMBRE", axis=1)
+        # Generate new columns
+        cols = {f"{col}" : f"{col}-{station}" for col in station_df.columns if col not in ["id"]}
+        station_df = station_df.rename(columns = cols)
+        # Append to list to join
+        dfs_for_stations.append(station_df)
+    # Join dfs
+    fun = lambda acc, el: pd.merge(acc, el, on="id", how="outer")
+    dfs = reduce(fun, dfs_for_stations)
+    dfs = dfs.dropna(axis=1) # Remove empty columns
+    return dfs
--- tests/test_process_data.py
+++ tests/test_process_data.py
@@ -1,13 +1,23 @@
+from numpy import fromstring
 import pytest
 from pca_renewables import read_files
 from pca_renewables.data_processing.process_data import parse_cammesa, parse_smn
+from datetime import datetime
+
+def _extract_day(x: str):
+    return datetime.strptime(x, "%d-%m-%y %H:%M").day
 
 def test_parse_cammesa() -> None:
     df = parse_cammesa("./tests/data")
-
     assert df.iloc[286].Eólico == 983.82 
-    assert (df.iloc[287].Momento.day - df.iloc[286].Momento.day) == 1
+    assert _extract_day(df.iloc[287].id) - _extract_day(df.iloc[286].id) == 1
 
 def test_parse_smn() -> None:
     df = parse_smn("./tests/data")
-    assert (df.iloc[2086].Momento.day - df.iloc[2085].Momento.day) == 1
+    assert _extract_day(df.iloc[24].id) - _extract_day(df.iloc[23].id) == 1
+
+def test_read_files() -> None:
+    df = read_files("./tests/data", "./tests/data")
+    for col in ["Eólico", "Fotovoltaico", "Bioenergías", "Hidráulico"]:
+        assert col in df.columns
+    assert df.shape[0] == 47
